f4f90b437f4e9160e544be62d9453f187ab1e010
wth7 <will.howerton@gmail.com>
2015-01-28 04:08:04 +0000

    Add nickname route

--- server.js
+++ server.js
@@ -5,4 +5,14 @@ app.get('/', function(request, response){
     response.send('Hello MGT-656!');
 });
 
+app.get('/netid', function(request, response){
+    response.send('YOU SHOULD PUT YOUR NETID HERE.');
+});
+
+app.get('/nickname', function(request, response){
+    response.send('calm yak');
+});
+
 app.listen(process.env.PORT || 4000);
+
+
